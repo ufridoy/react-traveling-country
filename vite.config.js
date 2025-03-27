71abc01e8c5722f7be6ccaf06255f36ee2394ea3
ufridoy <umorfarukridoy1@gmail.com>
2025-03-27 15:33:28 +0600

    Fixed Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./my-react-class-3/"
+  base: "./react-traveling-country/"
 })
